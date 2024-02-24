f8a43ea0c9b4ed9e09b0817dd5f3bea960250458
Aldi <asheftyaldia@gmail.com>
2024-02-24 21:11:09 +0700

    Mengubah dan menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba git hub video ke 8</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur.</p>
+</body>
+</html>
